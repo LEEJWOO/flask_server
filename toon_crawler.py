2ea7a2b733e043040f67118b5c9eed8b1c5239eb
LJW <shwarzschild@gmail.com>
2024-05-13 00:44:02 +0900

    ver. 0513 edited 0512 v2

--- toon_crawler.py
+++ toon_crawler.py
@@ -1,8 +1,10 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
 #from selenium.common.exceptions import WebDriverException
 
 import os
@@ -60,42 +62,48 @@ def star_crawler(titleId):
     driver = setup_driver()
     ratings = []
     page = 1
-    last_episode = 0
+    last_episode = 0  # 마지막 회차 번호를 추적하기 위한 변수
 
     try:
         while True:
             base_url = f'https://comic.naver.com/webtoon/list?titleId={titleId}&page={page}&sort=DESC'
             driver.get(base_url)
-            wait = WebDriverWait(driver, 10)
+            time.sleep(0.1)  # 페이지 로드를 보장하기 위해 잠시 대기 (동적 대기로 변경 예정)
 
-            # 변경: 요소가 클릭 가능할 때까지 대기
-            wait.until(EC.element_to_be_clickable((By.XPATH, '//div[@class="rating_type"]')))
             current_page_ratings = []
-            episodes = driver.find_elements(By.CSS_SELECTOR, 'td.title > a')
-
+            episodes = driver.find_elements(By.XPATH, '/html/body/div[1]/div/div[2]/div/div[1]/div[3]/ul/li/div/a')
             for episode in episodes:
-                episode_number = episode.get_attribute('href').split('no=')[1]
+                episode_number = int(episode.get_attribute('href').split('no=')[1].split('&')[0])
                 try:
-                    # 변경: 요소의 텍스트를 직접 접근
-                    star_rating = episode.find_element(By.XPATH, './following-sibling::td[@class="rating_type"]/strong').text
-                    current_page_ratings.append(float(star_rating))
-                    last_episode = max(last_episode, int(episode_number))
+                    xpath = f'/html/body/div[1]/div/div[2]/div/div[1]/div[3]/ul/li[{episode_number}]/a/div[2]/div/span[1]/span'
+                    star_rating = driver.find_element(By.XPATH, xpath).text
+                    if star_rating:
+                        current_page_ratings.append(float(star_rating))
+                        last_episode = max(last_episode, episode_number)
                 except Exception as e:
                     print(f"Error fetching rating for episode {episode_number}: {str(e)}")
                     continue
 
-            if len(current_page_ratings) < 20:  # 20개 미만인 경우 마지막 페이지로 간주
+            if len(current_page_ratings) < 20:  # 20개 미만이면 마지막 페이지로 간주, 루프 종료
                 ratings.extend(current_page_ratings)
                 break
 
             ratings.extend(current_page_ratings)
-            page += 1  # 다음 페이지로 이동
+            page += 1  # 다음 페이지로 넘어가기
 
     finally:
         driver.quit()
 
-    if not ratings:  # 별점 데이터가 없는 경우 처리
-        return None, 0
+    ratings.reverse()# 데이터를 역순으로 정렬하여 1화부터 마지막 화까지 순서대로 배열
+
+    # 결과를 파일에 저장
+    results_folder = os.path.join(os.getcwd(), 'Webtoon_Ratings')
+    os.makedirs(results_folder, exist_ok=True)
+    ratings_file_path = os.path.join(results_folder, f'{titleId}_ratings.txt')
+    with open(ratings_file_path, 'w', encoding='utf-8') as file:
+        for episode, rating in enumerate(ratings, 1):
+            file.write(f'{episode}, {rating}\n')
+            print(f'{episode}, {rating}')  # 콘솔에 출력
 
     return ratings, last_episode
 
